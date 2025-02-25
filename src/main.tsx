e424bdceac14ff3b0663b209dea3859b11848e19
kimhyojung <706shin1728@naver.com>
2025-02-25 22:28:03 +0900

    feat : 새고로침 시에도 캘린더 상태가 유지되게 위한 persist 설정 부분 추가

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,16 @@
+import { store, persistor } from "./store/index.ts";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <PersistGate loading={<div>로딩중,,</div>} persistor={persistor}>
+        <App />
+      </PersistGate>
+    </Provider>
   </StrictMode>
 );
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,10 +1,24 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import calendarReducer from "../reducers/calendar-slice";
 
-const store = configureStore({
-  reducer: {
-    calendar: calendarReducer,
-  },
+import storage from "redux-persist/lib/storage";
+import { persistReducer, persistStore } from "redux-persist";
+
+//새로고침 시에도 반영되게 세팅
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["calendar"],
+};
+
+const rootReducer = combineReducers({
+  calendar: calendarReducer,
 });
 
-export default store;
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+});
+
+export const persistor = persistStore(store);
