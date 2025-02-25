e2d71123f702cb7de5f4e83333b9feacc80b3300
kimhyojung <706shin1728@naver.com>
2025-02-26 01:19:59 +0900

    feat : getDateRange 함수 구현
    현재 날짜 - 같은 주의 일요일까지의 차이와
    현재 날짜 - 같은 주의 토요일까지 차이를 구해서 배열로 반환하는 함수

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,5 +1,6 @@
-import { format } from "date-fns";
+import { addDays, format, getDay, subDays } from "date-fns";
 import { ko } from "date-fns/locale";
+import { WEEK_LIST } from "../constants";
 
 export const renderCalendarCaptionYearMonth = ({ date }: { date: Date }) => {
   const year = format(date, "yyyy");
@@ -7,4 +8,23 @@ export const renderCalendarCaptionYearMonth = ({ date }: { date: Date }) => {
     locale: ko,
   });
   return `${year}년 ${month}월`;
+};
+
+export const getDateRange = ({ date }: { date: Date }) => {
+  const startDayofWeek = getDay(date);
+
+  const subDaysRange = Array.from({ length: startDayofWeek + 1 }).map(
+    (_, i) => {
+      return format(subDays(date, startDayofWeek - i), "dd");
+    }
+  );
+
+  const addDaysRange = Array.from(
+    { length: WEEK_LIST.length - startDayofWeek - 1 },
+    (_, i) => {
+      return format(addDays(date, i), "dd");
+    }
+  );
+
+  return [...subDaysRange, ...addDaysRange];
 };
