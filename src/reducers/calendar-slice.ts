dbd8bde0de1337bc0cb50a145a81f73a1e7f317d
kimhyojung <706shin1728@naver.com>
2025-02-25 22:37:29 +0900

    feat : 필요한 리듀서 모두 정의

--- src/reducers/calendar-slice.ts
+++ src/reducers/calendar-slice.ts
@@ -1,17 +1,35 @@
-import { createSlice } from "@reduxjs/toolkit";
-import { formatDate } from "date-fns";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { formatDate, subWeeks, format, addWeeks } from "date-fns";
+import type { Schedule } from "../types/schedule";
 
 const calendarSlice = createSlice({
   name: "calendar-slice",
   initialState: {
     date: formatDate(new Date(), "yyyy-MM-dd"),
-    schedule: [],
+    schedule: [] as Schedule[],
   },
   // 필요한 리듀서 정의
   reducers: {
-    shiftToPrevWeekReducer: () => {},
-    shiftToNextWeekReducer: () => {},
-    setDateReducer: () => {},
+    shiftToPrevWeekReducer: (state) => {
+      const prevWeekDate = subWeeks(new Date(state.date), 1);
+      state.date = format(prevWeekDate, "yyyy-MM-dd");
+    },
+    shiftToNextWeekReducer: (state) => {
+      const nextWeekDate = addWeeks(new Date(state.date), 1);
+      state.date = format(nextWeekDate, "yyyy-MM-dd");
+    },
+    setDateReducer: (state, action: PayloadAction<string>) => {
+      //유효한 날짜인지 검증
+      state.date = action.payload;
+    },
+    addNewScheduleReducer: (state, action: PayloadAction<Schedule>) => {
+      state.schedule.push(action.payload);
+    },
+    removeScheduleReducer: (state, action: PayloadAction<string>) => {
+      state.schedule = state.schedule.filter(
+        (schedule) => schedule.id !== action.payload
+      );
+    },
   },
 });
 
