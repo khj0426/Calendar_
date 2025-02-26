c3a4c490ff0300d6290b6eb741047c845e042a0b
kimhyojung <706shin1728@naver.com>
2025-02-27 01:02:07 +0900

    style : 일정 제목 길어질 떄의 처리 추가

--- src/components/modal/ScheduleDetailModal.tsx
+++ src/components/modal/ScheduleDetailModal.tsx
@@ -8,8 +8,8 @@ export const ScheduleDetailModal = () => {
 
   return (
     <div aria-label="schedule-detail-modal" className="h-full">
-      <div className="flex flex-col gap-4 p-6 z-10 bg-white w-[400px] h-auto rounded-lg shadow-lg fixed overflow-hidden top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 border border-gray-300">
-        <h2 className="text-lg font-semibold text-gray-800">
+      <div className="flex flex-col gap-4 p-6 z-10 bg-white max-w-[400px] h-auto rounded-lg shadow-lg fixed flex-wrap top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 border border-gray-300">
+        <h2 className="text-lg font-semibold text-gray-800 break-words w-5/6">
           {activeSchedule.title}
         </h2>
         <div className="text-sm text-gray-600">
