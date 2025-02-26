b20628201d1eaa7ea07231e4b83a4012dc6cbe62
kimhyojung <706shin1728@naver.com>
2025-02-26 13:35:41 +0900

    feat : 헤더에 border스타일 추가

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -15,7 +15,7 @@ export const Button = ({
   return (
     <button
       className={cn(
-        "rounded-2xl border-2 border-gray-300 bg-white text-black h-[56px] w-[128px] flex justify-center items-center gap-4 cursor-pointer",
+        "rounded-2xl border-2 border-gray-300 text-black h-[56px] w-[128px] flex justify-center items-center gap-4 cursor-pointer",
         className
       )}
       {...rest}
--- src/components/header/CalendarHeader.tsx
+++ src/components/header/CalendarHeader.tsx
@@ -18,7 +18,7 @@ interface CalendarHeaderProps {
 export const CalendarHeader = ({ toggleSidebar }: CalendarHeaderProps) => {
   const dispatch = useAppDispatch();
   return (
-    <header className="w-full h-[48px] bg-[#f8fafd] flex items-center mt-2.5">
+    <header className="w-full h-12 bg-[#f8fafd] p-4 flex items-center border-b border-gray-300">
       <div className="px-4 flex gap-4 items-center ml-4">
         <FaAlignLeft
           size={25}
@@ -30,14 +30,14 @@ export const CalendarHeader = ({ toggleSidebar }: CalendarHeaderProps) => {
 
       <div>
         <Button
-          className="ml-6 w-[74px] border-[1px]"
+          className="ml-6 w-[74px] border-[1px] h-[32px]"
           onClick={() => dispatch(setDateReducer(new Date()))}
         >
           오늘
         </Button>
       </div>
 
-      <div className="flex items-center gap-4 ml-8 cursor-pointer">
+      <div className="flex items-center gap-4 ml-8 cursor-pointer h-full">
         <FaAngleLeft onClick={() => dispatch(shiftToPrevWeekReducer())} />
         <FaAngleRight onClick={() => dispatch(shiftToNextWeekReducer())} />
       </div>
